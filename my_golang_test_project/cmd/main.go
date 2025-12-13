13248b0423f5d5d855dcd58f842d85dd9640b11e
Lee Deckert <leedeck21@users.noreply.github.com>
2025-12-13 07:58:54 +0000

    WIP - Making snake work

--- my_golang_test_project/cmd/main.go
+++ my_golang_test_project/cmd/main.go
@@ -10,8 +10,13 @@ import (
 
 func main() {
 	// We want to run the snake game multiple times concurrently to see which player wins most often.
+	winnersChannel := make(chan int)
 
-	playSnakeGame("Lee")
+	for i := 0; i < 100; i++ {
+		fmt.Printf("Starting game %d\n", i+1)
+		go playSnakeGame(winnersChannel)
+		fmt.Println()
+	}
 
 	// I need a snake game
 	// A way to run multiple games at the same time
@@ -25,22 +30,43 @@ func diceRoll() int {
 
 // isSnake and isLadder moved to gameutils package
 
-func playSnakeGame(playerName string) {
-	playerPosition := 0
-
-	for i := 0; i < 100; i++ {
-		playerPosition = takeTurn(playerName, playerPosition)
+func playSnakeGame(winnersChannel chan int) {
+	playerPositions := map[int]int{
+		1: 0,
+		2: 0,
+		3: 0,
+		4: 0,
 	}
 
-	fmt.Printf("Player position is %d", playerPosition)
+	winner := 0
 
+	for winner == 0 {
+		for playerNumber, playerPosition := range playerPositions {
+			newPosition := takeTurn(playerPosition)
+			playerPositions[playerNumber] = newPosition
+
+			if newPosition == 100 {
+				winner = playerNumber
+
+				break
+			}
+		}
+	}
+
+	fmt.Printf("Game winner is %d", winner)
+	winnersChannel <- winner
 }
 
-func takeTurn(playerName string, currentPosition int) int {
+func takeTurn(currentPosition int) int {
 	diceRollOutcome := diceRoll()
-	currentPosition += diceRollOutcome
-	currentPosition = gameutils.IsSnake(currentPosition)
-	currentPosition = gameutils.IsLadder(currentPosition)
+	newPosition := diceRollOutcome + currentPosition
 
-	return currentPosition
+	if newPosition > 100 {
+		return currentPosition
+	}
+
+	newPosition = gameutils.IsSnake(newPosition)
+	newPosition = gameutils.IsLadder(newPosition)
+
+	return newPosition
 }
